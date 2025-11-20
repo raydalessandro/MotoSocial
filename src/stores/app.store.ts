b302ab032aa9aba2cee974c0ff800bba871196f3
raydalessandro <alessio.marrone1993@gmail.com>
2025-11-20 15:48:55 +0100

    Update app.store.ts

--- src/stores/app.store.ts
+++ src/stores/app.store.ts
@@ -1,30 +1,32 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 import { User, Post, Route } from '@/types/models';
 
 type AppState = {
-  // Auth
   user: User | null;
   isAuthenticated: boolean;
-  
-  // Feed
   posts: Post[];
-  
-  // Routes
   routes: Route[];
   
-  // Actions
   setUser: (user: User | null) => void;
   addPost: (post: Post) => void;
   addRoute: (route: Route) => void;
 };
 
-export const useAppStore = create<AppState>((set) => ({
-  user: null,
-  isAuthenticated: false,
-  posts: [],
-  routes: [],
-  
-  setUser: (user) => set({ user, isAuthenticated: !!user }),
-  addPost: (post) => set((state) => ({ posts: [post, ...state.posts] })),
-  addRoute: (route) => set((state) => ({ routes: [...state.routes, route] })),
-}));
+export const useAppStore = create<AppState>()(
+  persist(
+    (set) => ({
+      user: null,
+      isAuthenticated: false,
+      posts: [],
+      routes: [],
+      
+      setUser: (user) => set({ user, isAuthenticated: !!user }),
+      addPost: (post) => set((state) => ({ posts: [post, ...state.posts] })),
+      addRoute: (route) => set((state) => ({ routes: [...state.routes, route] })),
+    }),
+    {
+      name: 'motosocial-storage',
+    }
+  )
+);
